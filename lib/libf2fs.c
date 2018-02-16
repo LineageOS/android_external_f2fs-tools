6e678126a5b08ec288c2de28b65ef0ceeecd2e8f
Tom Marshall <tdm.code@gmail.com>
2018-02-17 00:44:40 +0100

    f2fs-tools: Rename utf conversion symbols
    These clash with libbase
    
    Change-Id: I74162d1fcbdb14dbfdfc8d7252a7d425a146bd4a

--- lib/libf2fs.c
+++ lib/libf2fs.c
@@ -118,7 +118,7 @@ static u_int16_t *wchar_to_utf16(u_int16_t *output, wchar_t wc, size_t outsize)
 	return output + 2;
 }
 
-int utf8_to_utf16(u_int16_t *output, const char *input, size_t outsize,
+int f2fs_utf8_to_utf16(u_int16_t *output, const char *input, size_t outsize,
 		size_t insize)
 {
 	const char *inp = input;
@@ -204,7 +204,7 @@ static char *wchar_to_utf8(char *output, wchar_t wc, size_t outsize)
 	return output;
 }
 
-int utf16_to_utf8(char *output, const u_int16_t *input, size_t outsize,
+int f2fs_utf16_to_utf8(char *output, const u_int16_t *input, size_t outsize,
 		size_t insize)
 {
 	const u_int16_t *inp = input;
